579cc47f201f6f795331a94625a9bf68164d9d6f
Andrei Mihai <andreimihai2305is@gmail.com>
2024-09-26 10:27:23 +0300

    modified project structure

--- data_structures/build.sh
+++ data_structures/build.sh
@@ -7,7 +7,7 @@ if [ ! -d "./build" ]; then
 
     mkdir ./build
 fi
-cc -o ./build/dynamic_array ./dynamic_array.c
-cc -o ./build/linked_list ./linked_list.c
-cc -o ./build/queue ./queue.c
-cc -o ./build/main ./main.c
+cc -o ./build/dynamic_array ./dynamic-array/dynamic_array.c
+cc -o ./build/linked_list ./linked-list/linked_list.c
+cc -o ./build/queue -I./dynamic-array/ ./queue/queue.c
+cc -o ./build/main -I./dynamic-array/ ./main.c
